7d1de7387368d1dd4ddfac4b15b26b4685ba1cf5
Vic <Vic@gmail.com>
2020-03-06 16:51:02 +0800

    修正__互動標籤與卡片hover

--- js/all.js
+++ js/all.js
@@ -1,16 +1,27 @@
 
 $(document).ready(function() {
-  $('.fa-heart').on('click',  function(){
-      $(this).toggleClass('fas text-danger');
-  });
-  $('.btn-outline-danger').on('click',  function(){
-      $(".far").removeClass('fas text-danger');
+  $('.innerProduct__item__like').on('click',  function(){
+      $(this).find('.far').toggleClass('fas text-danger');
   });
   $('.top').on('click',  function(){
     event.preventDefault();
     $('html,body').animate({ scrollTop: 0 }, 1000);
   });
+  $('.card-img-top, .card-body, .innerProduct__item::before').hover(function(){
+    $(this).parent().toggleClass('innerProduct__item__active');
+  });
+  $('.card-body .btn').hover(function(){
+    $('.card-body').parent().toggleClass('innerProduct__item__active');
+  });
   
   
+
+
+  
+  // $('.card-img-top').on('click',  function(){
+  //   $(this).parent().toggleClass('innerProduct__item__active').siblings()
+	// 	.removeClass('innerProduct__item__active').find().toggleClass('innerProduct__item__active');
+    
+  // });
 
 });
